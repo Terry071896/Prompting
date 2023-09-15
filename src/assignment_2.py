ceb55fbc679072b1ff67ecb13dabead46fe2e27f
tc32856 <terry.cox@aero.org>
2023-09-14 23:51:04 -0700

    fixed prompts and able to run

--- src/assignment_2.py
+++ src/assignment_2.py
@@ -1,8 +1,8 @@
 import argparse
 import numpy as np
 import torch
-import jsonlines 
-import random 
+import jsonlines
+import random
 
 def newyorker_caption_contest_data(args):
     from datasets import load_dataset
@@ -16,16 +16,16 @@ def newyorker_caption_contest_data(args):
             inp = inst['from_description']
             targ = inst['label']
             cur_spl.append({'input': inp, 'target': targ, 'instance_id': inst['instance_id'], 'image': inst['image'], 'caption_choices': inst['caption_choices']})
-        
+
             #'input' is an image annotation we will use for a llama2 e.g. "scene: the living room description: A man and a woman are sitting on a couch. They are surrounded by numerous monkeys. uncanny: Monkeys are found in jungles or zoos, not in houses. entities: Monkey, Amazon_rainforest, Amazon_(company)."
-            #'target': a human-written explanation 
+            #'target': a human-written explanation
             #'image': a PIL Image object
             #'caption_choices': is human-written explanation
 
         res[spl_name] = cur_spl
     return res
 
-def newyorker_caption_contest_idefics(args): 
+def newyorker_caption_contest_idefics(args):
     from transformers import IdeficsForVisionText2Text, AutoProcessor
 
     print("Loading model")
@@ -44,7 +44,7 @@ def newyorker_caption_contest_idefics(args):
     for val_inst in nyc_data_five_val:
         # ======================> ADD YOUR CODE TO DEFINE A PROMPT WITH TWO TRAIN EXAMPLES/DEMONSTRATIONS/SHOTS <======================
         # Each instace has a key 'image' that contains the PIL Image. You will give that to the model as input to "show" it the image instead of an url to the image jpg file.
-        
+
         prompt = [
         "\nUser: What is the caption of this image?",
         nyc_data_train_two[0]['image'],
@@ -52,7 +52,7 @@ def newyorker_caption_contest_idefics(args):
 
         "\nAssistant: %s<end_of_utterance>"%nyc_data_train_two[0]['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
         "\nAssistant: %s<end_of_utterance>"%nyc_data_train_two[0]['target'],
@@ -63,10 +63,10 @@ def newyorker_caption_contest_idefics(args):
 
         "\nAssistant: %s<end_of_utterance>"%nyc_data_train_two[1]['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
-        "\nAssistant: %s<end_of_utterance>"%nyc_data_train_two[0]['target'],
+        "\nAssistant: %s<end_of_utterance>"%nyc_data_train_two[1]['target'],
 
         "\nUser: What is the caption of this image?",
         val_inst['image'],
@@ -74,7 +74,7 @@ def newyorker_caption_contest_idefics(args):
 
         "\nAssistant: %s<end_of_utterance>"%val_inst['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
         "\nAssistant:",
@@ -82,7 +82,7 @@ def newyorker_caption_contest_idefics(args):
         print(prompt)
 
         prompts.append(prompt)
-        
+
         # I'm saving images to `out`` to be able to see them in the output folder
         val_inst['image'].save(f"out/{val_inst['instance_id']}.jpg")
 
@@ -114,9 +114,9 @@ def newyorker_caption_contest_idefics(args):
         for item in nyc_data_train_two:
             del item['image']
             writer.write(item)
-        
 
-def newyorker_caption_contest_llama2(args): 
+
+def newyorker_caption_contest_llama2(args):
     print ("Loading data")
     nyc_data_five_val = []
     with jsonlines.open('out/val.jsonl') as reader:
@@ -133,12 +133,12 @@ def newyorker_caption_contest_llama2(args):
 
     print("Loading model")
     '''
-    Ideally, we'd do something similar to what we have been doing before: 
+    Ideally, we'd do something similar to what we have been doing before:
 
         tokenizer = AutoTokenizer.from_pretrained(args.llama2_checkpoint, padding_side="left")
         model = AutoModelForCausalLM.from_pretrained(args.llama2_checkpoint, torch_dtype=torch.float16, device_map="auto")
         tokenizer.pad_token = tokenizer.unk_token_id
-        
+
         prompts = [ "our prompt" for val_inst in nyc_data_five_val]
 
         inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(model.device)
@@ -146,8 +146,8 @@ def newyorker_caption_contest_llama2(args):
         output_sequences = model.generate(**inputs, max_new_tokens=1024, do_sample=False)
         generated_text = [tokenizer.decode(s, skip_special_tokens=True) for s in output_sequences]
 
-    But I cannot produce text with this prototypical code with HF llama2. 
-    Thus we will use pipeline instead. 
+    But I cannot produce text with this prototypical code with HF llama2.
+    Thus we will use pipeline instead.
     '''
     import transformers
     from transformers import AutoTokenizer
@@ -160,7 +160,7 @@ def newyorker_caption_contest_llama2(args):
         device_map="auto",
     )
 
-    for i, val_inst in enumerate(nyc_data_five_val):         
+    for i, val_inst in enumerate(nyc_data_five_val):
         # ======================> ADD YOUR CODE TO DEFINE A PROMPT WITH TWO TRAIN EXAMPLES/DEMONSTRATIONS/SHOTS <======================
         prompt = ''.join([
         nyc_data_train_two[0]['input'],
@@ -171,7 +171,7 @@ def newyorker_caption_contest_llama2(args):
 
         "\nAssistant: %s<end_of_utterance>\n"%nyc_data_train_two[0]['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
         "\nAssistant: %s<end_of_utterance>\n"%nyc_data_train_two[0]['target'],
@@ -185,7 +185,7 @@ def newyorker_caption_contest_llama2(args):
 
         "\nAssistant: %s<end_of_utterance>\n"%nyc_data_train_two[1]['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
         "\nAssistant: %s<end_of_utterance>\n"%nyc_data_train_two[1]['target'],
@@ -198,7 +198,7 @@ def newyorker_caption_contest_llama2(args):
 
         "\nAssistant: %s<end_of_utterance>\n"%val_inst['caption_choices'],
 
-        "\nUser: What is an explaination for this caption?",
+        "\nUser: What is an explanation for this caption?",
         "<end_of_utterance>",
 
         "\nAssistant:",
@@ -210,7 +210,7 @@ def newyorker_caption_contest_llama2(args):
             eos_token_id=tokenizer.eos_token_id,
             max_length=1024,
         )
-        
+
         gen_expl = sequences[0]['generated_text'].split("/INST] ")[-1]
         nyc_data_five_val[i]['generated_llama2']=gen_expl
 
@@ -222,14 +222,15 @@ def newyorker_caption_contest_llama2(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, help='Random seed set to your uNID') # <======================> 
+    parser.add_argument('--seed', type=int, help='Random seed set to your uNID') # <======================>
     parser.add_argument('--output_dir', type=str, help='Directory where model checkpoints will be saved')
-    parser.add_argument('--task_name', default="jmhessel/newyorker_caption_contest",  type=str, help='Name of the task that will be used by huggingface load dataset')    
+    parser.add_argument('--task_name', default="jmhessel/newyorker_caption_contest",  type=str, help='Name of the task that will be used by huggingface load dataset')
     parser.add_argument('--subtask', default="explanation", type=str, help="The contest has three subtasks: matching, ranking, explanation")
     parser.add_argument('--idefics_checkpoint', default="HuggingFaceM4/idefics-9b-instruct", type=str, help="The hf name of an idefics checkpoint")
     parser.add_argument('--llama2_checkpoint', default="meta-llama/Llama-2-7b-chat-hf", type=str, help="The hf name of a llama2 checkpoint")
     args = parser.parse_args()
-
+    import random
+    random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
